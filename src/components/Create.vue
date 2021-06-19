853baab16ffe29b3248528da78b2150f0f6ea850
trinhlh96 <trinhlhth1908021@fpt.edu.vn>
2021-06-19 15:55:09 +0700

    first commit

---
+++ src/components/Create.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3>Add employees</h3>
+            </div>
+            <div class="card-body">
+                <form v-on:submit.prevent="addItem">
+                  <div class="form-group">
+                    <label>Name:</label>
+                    <input type="text" class="form-control" v-model="item.name"/>
+                  </div>
+                  <div class="form-group">
+                    <label>Wage:</label>
+                    <input type="text" class="form-control" v-model="item.wage"/>
+                  </div>
+
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Add employees"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+      name: 'AddItem'
+  },
+  data() {
+      return {
+          item: {}
+      }
+  },
+  methods: {
+      addItem() {
+           let uri = 'http://localhost:8089/employees/create';
+            this.axios.post(uri, this.item).then((response) => {
+              this.$router.push({name: 'Index'});
+            });
+        }
+    }
+}
+</script>
---
+++ src/components/Index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>employees</h1>
+
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Wage</td>
+            </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="item in items" :key="item.id">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.wage }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data(){
+            return{
+                items: []
+            }
+        },
+
+        created: function()
+        {
+            this.fetchItems();
+        },
+
+        methods: {
+            fetchItems()
+            {
+              let uri = 'http://localhost:8089/employees/index';
+              this.axios.get(uri).then((response) => {
+                  this.items = response.data;
+                  console.log(this.items);
+              });
+            },
+        }
+    }
+</script>
